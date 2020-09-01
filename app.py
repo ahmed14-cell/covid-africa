b7cd35ce27aa9d5df2634e04f9a9df4b0520dc98
ahmed14-cell <ahmedito14@gmail.com>
2020-09-01 14:24:14 +0300

    added extras

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import dash_bootstrap_components as dbc
 # https://bootswatch.com/lux/
 external_stylesheets = [dbc.themes.LUX, 'assets/style.css', 'assets/font.css']
 
+server = app.server
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 #if __name__ =='__main':
 #    app.run_server(port=2020, debug=True)
 
 app.title = 'COVID-19 in Africa'
-server = app.server
 app.config.suppress_callback_exceptions = True

715d345fe17885de299f7755024d766da8c85db3
ahmed14-cell <ahmedito14@gmail.com>
2020-09-03 12:16:03 +0300

    now

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import dash_bootstrap_components as dbc
 
 # bootstrap theme
 # https://bootswatch.com/lux/
-external_stylesheets = [dbc.themes.LUX, 'assets/style.css']
+external_stylesheets = [dbc.themes.LUX]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
